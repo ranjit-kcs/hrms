1e27cf44f08918f19e7d72e1b05aeb2a9bd46a0d
ravish-gowda <ravisha.jh@weassist.co.in>
2026-06-20 12:38:36 +0530

    feat: implement login and device ID reset functionality

---
+++ hrms/api/custom_login.py
@@ -0,0 +1,34 @@
+import frappe
+# from frappe.auth import LoginManager
+
+
+@frappe.whitelist(allow_guest=True)
+def login(email, password, device_id):
+    user = frappe.db.get_value("User", {"email": email}, ["name", "device_id"], as_dict=True)
+
+    if not user:
+        frappe.throw("Invalid credentials")
+
+    # DEVICE CHECK BEFORE LOGIN
+    if user.device_id and user.device_id != device_id:
+        frappe.throw("You are already logged in from another mobile. Contact your HR.")
+
+    # NOW authenticate user
+    frappe.local.login_manager.authenticate(user=user.name, pwd=password)
+    frappe.local.login_manager.post_login()
+
+    # Save device_id first time
+    if not user.device_id:
+        frappe.db.set_value("User", user.name, "device_id", device_id)
+
+    return {
+        "status": "device_match" if user.device_id else "first_login",
+        "message": "Logged In",
+        "home_page": "/hrms/home"
+    }
+
+@frappe.whitelist()
+def reset_user_device_id(user):
+    if device_id := frappe.db.get_value("User", user, "device_id"):
+        frappe.db.set_value("User", user, "device_id", None)
+        return {"message": "Device ID reset successfully."}
